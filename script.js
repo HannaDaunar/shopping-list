8a8182b67b8b75c5c002c3997298908db50f9f48
HannaDaunar <hannadaunar@yandex.by>
2024-09-27 13:16:03 +0200

    Update script.js
    created event listeners

--- script.js
+++ script.js
@@ -7,3 +7,22 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+input.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const itemText = input.value;
+        const newItem = document.createElement('div');
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        })
+        newItem.textContent = itemText;
+        if (itemText != '') {
+            items.append(newItem);
+        }
+        input.value = '';
+
+    }
+})
